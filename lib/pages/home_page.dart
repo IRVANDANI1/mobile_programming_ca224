8d9947d881884bf4ea65b3c3756ed292aa09681e
1RV4ND4N1 <irvandani862@gmail.com>
2024-11-10 07:20:28 +0800

    commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_programming_ca224/models/moment.dart';
 import 'package:mobile_programming_ca224/widgets/post_item.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({super.key, required this.moments});
+  final List<Moment> moments;
 
   @override
   Widget build(BuildContext context) {
-    final listPostItems = List.generate(15, (index) => const PostItem());
+    final listPostItems = moments
+        .map(
+          (momentItem) => PostItem(
+            moment: momentItem,
+          ),
+        )
+        .toList();
 
     return SingleChildScrollView(
       child: Column(
--- lib/widgets/post_action_list.dart
+++ lib/widgets/post_action_list.dart
@@ -3,23 +3,31 @@ import 'package:flutter/material.dart';
 import 'post_action.dart';
 
 class PostActionList extends StatelessWidget {
-  const PostActionList({super.key});
+  const PostActionList({
+    super.key,
+    this.likeCount = 0,
+    this.commentCount = 0,
+    this.bookmarkCount = 0,
+  });
+  final int likeCount;
+  final int commentCount;
+  final int bookmarkCount;
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
         PostAction(
           asset: 'assets/icons/fi-br-heart.svg',
-          label: '1.2k',
+          label: likeCount.toString(),
         ),
         PostAction(
           asset: 'assets/icons/fi-br-comment.svg',
-          label: '254',
+          label: commentCount.toString(),
         ),
         PostAction(
           asset: 'assets/icons/fi-br-bookmark.svg',
-          label: '55',
+          label: bookmarkCount.toString(),
         ),
       ],
     );
